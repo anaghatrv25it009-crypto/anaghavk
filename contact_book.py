9c820fb688a01ee40508c398dfaa47f97225a7d5
anaghatrv25it009-crypto <anagha.trv25it009@gecbh.ac.in>
2026-02-09 00:21:33 +0530

    Add files via upload

---
+++ contact_book.py
@@ -0,0 +1,8 @@
+name=str(input("enter your name:"))
+email=str(input("enter your email:"))
+phonenumber=int(input("enter your phone number:"))
+print("ALL YOUR DATAS ARE VERIFYING")
+print("Hey..your name is",name)
+
+print("your email is",email)
+print("your number is",phonenumber)
